9e13f5a8d71cc47c326e53ecd3777ed930096e96
easygautam <gautam90055@gmail.com>
2019-11-30 16:20:50 +0530

    Lazy notification image loading

--- app/src/main/java/com/example/demo/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/demo/MyFirebaseMessagingService.kt
@@ -5,54 +5,58 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
-
+/**
+ * Service class to handle Firebase events like- receive message and device token change
+ */
 class MyFirebaseMessagingService : FirebaseMessagingService() {
+
     private val TAG = javaClass.simpleName
-    override fun onMessageReceived(remoteMessage: RemoteMessage) { // ...
-// TODO(developer): Handle FCM messages here.
-// Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+
+    /**
+     * This method will be called on message received sent by server or anywhere else on FCM
+     */
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+
         Log.d(TAG, "From: " + remoteMessage.from)
+
         // Check if message contains a data payload.
         if (remoteMessage.data.isNotEmpty()) {
             Log.d(TAG, "Message data payload: " + remoteMessage.data)
-            //            if (/* Check if data needs to be processed by long running job */ true) {
-//                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
-//                scheduleJob();
-//            } else {
-//                // Handle message within 10 seconds
-//                handleNow();
-//            }
 
-            sendNotification(remoteMessage.data["title"]!!, remoteMessage.data["text"]!!)
-        }
-        // Check if message contains a notification payload.
-        if (remoteMessage.notification != null) {
-            Log.d(
-                TAG,
-                "Message Notification Body: " + remoteMessage.notification!!.body
+            // Show notification
+            sendNotification(
+                remoteMessage.data["title"]!!,
+                remoteMessage.data["text"]!!,
+                remoteMessage.data["image"]!!
             )
         }
-        // Also if you intend on generating your own notifications as a result of a received FCM
-// message, here is where that should be initiated. See sendNotification method below.
+
     }
 
+    /**
+     * This method will be called on device token refreshed (See Firebase device token change policy for more details)
+     */
     override fun onNewToken(token: String) {
         Log.d(TAG, "Refreshed token: $token")
-        // If you want to send messages to this application instance or
-// manage this apps subscriptions on the server side, send the
-// Instance ID token to your app server.
-//        sendRegistrationToServer(token);
+
+        // Update token on server or store where you want
     }
 
 
-
-    private fun sendNotification(title: String, message: String) {
+    /**
+     * Show notification to user with title, description and notification image
+     */
+    private fun sendNotification(title: String, message: String, imageUrl: String) {
 
         // Sets an ID for the notification, so it can be updated.
         val notifyID = 1
@@ -64,16 +68,19 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         val mNotificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create notification channel its required with and after oreo
             val importance = NotificationManager.IMPORTANCE_HIGH
             val mChannel = NotificationChannel(CHANNEL_ID, name, importance)
             mNotificationManager.createNotificationChannel(mChannel)
         }
 
+        // Define a intent for a task that you want to perform on notification click i.e - start activity, etc.
         val emptyIntent = Intent()
         val pendingIntent =
             PendingIntent.getActivity(this, 1, emptyIntent, PendingIntent.FLAG_UPDATE_CURRENT)
 
-        val mBuilder =  NotificationCompat.Builder(this,  CHANNEL_ID)
+        // Build a notification
+        val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(R.mipmap.ic_launcher)
             .setContentTitle(title)
             .setContentText(message)
@@ -82,6 +89,28 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.notify(notifyID, mBuilder.build())
+
+        /**
+         * Load image in bitmap from url
+         */
+        Glide.with(this)
+            .asBitmap()
+            .load(imageUrl)
+            .into(object : CustomTarget<Bitmap>() {
+                override fun onResourceReady(
+                    resource: Bitmap,
+                    transition: com.bumptech.glide.request.transition.Transition<in Bitmap>?
+                ) {
+                    Log.d(TAG, "Image loaded")
+                    // add large image into notification
+                    mBuilder.setLargeIcon(resource)
+                    mBuilder.setStyle(NotificationCompat.BigPictureStyle().bigPicture(resource))
+                    // update notification with image
+                    notificationManager.notify(notifyID, mBuilder.build())
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) {}
+            })
 
     }
 
